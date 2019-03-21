7fb64c52d07a91e97c4fd8e784ee6eed3acbb5bb
Quentin <Cavle21@users.noreply.github.com>
2019-03-20 21:10:18 -0400

    Update Edit-TimeSheet.ps1
    Finished functionality for get-value.

--- Edit-TimeSheet.ps1
+++ Edit-TimeSheet.ps1
@@ -1,26 +1,53 @@
+<#
+
+.PARAMETER $mode
+    Get-Hours, Set-Hours
+
+.PARAMETER $contractNumber
+    The full number for our contract work Auths
+
+.PARAMETER $hoursToAdd
+    Hours to add to your store
+
+.NOTES
+HardCodedValues
+    Get-Value row 5
+        $Column = $Worksheet.Cells.Item(5, $Col).Value().trim()
+    Get-Value row 1
+        While ($Worksheet.Cells.Item(1,$Col).Value() -ne $null)
+#>
 
 param (
     [Parameter(Mandatory=$true,ParameterSetName='Mode')]
     [ValidateSet("Get-Hours", "Set-Hours")]
     [ValidateNotNullOrEmpty()]
         [string]$mode,
-    [Parameter(Mandator=$true)]
+    [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
-        [string]$contractNumber, ######-###-###-###
+    [ValidatePattern(“\d{6}-\d{3}-\d{3}-\d{3}”)]
+        [string]$contractNumber,
     [Parameter(ParameterSetName='Set-Hours')]
-        [uint32]$hoursToAdd
+        [uint32]$hoursToAdd,
+    [Parameter(ParameterSetName='ValueToChange')]
+        [string]$valueToChange,
+    [Parameter(ParameterSetName='Value')]
+        [string]$value,
+    [Parameter()]
+    [ValidateScript({
+        try {
+            Test-Path -Path $_ -ErrorAction stop
+        } catch {
+            Throw "${_}"
+        }
+    })]
+        [string]$path = "$env:userprofile\documents\personaldocs\Work Authorizations.xlsx"
 )
-
-$path = "$env:userprofile\documents\personaldocs\Work Authorizations.xlsx"
-
-$contractNumbers = $contractNumber.Split("-")
-
-function Import-ExcelFile {
+function Import-MyExcelFile {
 
     param(    
         [parameter()]
-            $file = "$env:userprofile\documents\personaldocs\Work Authorizations.xlsx",
-        [prameter()]
+            $path = "$env:userprofile\documents\personaldocs\Work Authorizations.xlsx",
+        [parameter()]
             $page = 'Sheet1'
     )
 
@@ -35,23 +62,26 @@ function Import-ExcelFile {
 
     $workSheet = $workBook.worksheets | Where-Object {$_.Name -eq $page}
 
-    return $worksheet
+    $excelObjArr = @($excel, $workbook, $workSheet)
 
+    return $excelObjArr
 }
 
 function Get-Value {
+
     param (
-        $contractNumbers
+        [System.Collections.ArrayList]$contractNumbers,
+        [System.Collections.ArrayList]$excelObjArr
     )
 
     $result = [PSCustomObject]@{}
     $numberOfResults = 1
     $firstRow = $found.row
     $foundArr = new-object System.Collections.ArrayList
-
+    #find all rows that match the first set of numbers
     $found = $worksheet.Cells.Find($contractNumbers[0])
-
     $loop = $true
+    #loop through all results $found and store them in an array
     while($loop -eq $true){
         $foundarr.Add($found)
         $found = $worksheet.Cells.FindNext($found)
@@ -63,6 +93,8 @@ function Get-Value {
         }
     }
 
+    #check rows found against second element in array ( hopefully this eliminates all others but one)
+
     forEach ($item in $foundarr){
         if ($workSheet.cells.item($item.row,10).text -eq $contractNumbers[1]){
             $found = $item
@@ -72,25 +104,65 @@ function Get-Value {
     $col = 1
 
     $ArrHeaders = new-object System.Collections.ArrayList
-
+    #get all column headers
     Do  { 
         $Column = $Worksheet.Cells.Item(5, $Col).Value().trim()  
         $ArrHeaders += $Column -replace " ", "" 
         $intCol++ 
-    }While ($Worksheet.Cells.Item(1,$Col).Value() -ne $null)
+    } While ($Worksheet.Cells.Item(1,$Col).Value() -ne $null)
     
     $col = 1
-
-    foreach ($objHeader in $ArrHeaders){  
-        IF ($objWorksheet.Cells.Item($found.Row, $col).Value() -ne $null)  
-        {  
-                $result | Add-Member -type NoteProperty -name $objHeader -value $worksheet.Cells.Item($found.Row, $Col).Value().trim()  
+    #add data to result object using column headers as variable name
+    forEach ($objHeader in $ArrHeaders){  
+        If ($objWorksheet.Cells.Item($found.Row, $col).Value() -ne $null){  
+            $result | Add-Member -type NoteProperty -name $objHeader -value $worksheet.Cells.Item($found.Row, $Col).Value().trim()  
         }else{ 
-                $result | Add-Member -type NoteProperty -name $objHeader -value $null  
-        } 
+            $result | Add-Member -type NoteProperty -name $objHeader -value $null  
+        }
+
         $Col++ 
     }  
-
+    #pscustomObj
     return $result
+}
+
+Function Set-Value {
+    param(
+        [object]$GetValueResultObj,
+        [string]$valueToChange = "HoursRemaining",
+        [string]$value,
+        [System.Collections.ArrayList]$excelObjArr
+    )
+
+    if($GetValueResultObj.$valueToChange -eq $null){
+        throw "$valueToChange parameter does not exist in the result object"
+        exit
+    }
+
+    $GetValueResultObj.$valueToChange = $value
+
+    
+
+}
+
+function Main {
+
+    $contractNumbers = $contractNumber.Split("-")
+
+    $excelObjArr = Import-MyExcelFile
+
+    switch($mode){
+        "Get-Hours"{
+            $results = Get-Value -contractNumbers $contractNumbers
+            Write-Host "For $contractNumber you have {0} hours remaining" -f $results.HoursRemaining
+        }
+        "Set-Hours"{
+            $results = Get-Value -contractNumbers $contractNumbers
+        }
+    }
+
+    
+
+
 }
 
